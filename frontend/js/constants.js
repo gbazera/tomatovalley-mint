fd2b2eb44028a6756cb30181dcc6b5e670050e62
gbazera <bazerashvili.gio27@gmail.com>
2022-05-07 14:35:52 +0400

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,8 +1,8 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x6680dE72260fe60DE951520e5533288a0a75d66E"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
-const welcome_h2 = "Connect to MetaMask to Get Started";
+const welcome_h1 = "Welcome to Tomato Valley!";
+const welcome_h2 = "Connect to Metamask to get started";
 const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
